62ada15b36781a21cf1e48564b112453788fb15f
Woodye14 <118812533+Woodye14@users.noreply.github.com>
2023-07-14 14:43:52 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -27,7 +27,8 @@ docker run -d \
     --name nginx \
     -p 80:80 \
     --network trio-task-network \
-    --mount type=bind,source=$(pwd)/nginx/nginx.conf,target=/etc/nginx/nginx.conf
+    --mount type=bind,source=$(pwd)/nginx/nginx.conf,target=/etc/nginx/nginx.conf \
+    nginx
 
 echo
 docker ps -a
